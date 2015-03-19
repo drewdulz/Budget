83bfafad451580737d418ac2d55276aec7dd0cea
Drew Warkentin <drewdulz@gmail.com>
2015-03-19 10:24:01 -0400

    Revert c67a2ee..1986935
    This rolls back to commit c67a2eee4760bd36d0d40d0cf61f6e8561b8b0d5.

--- budget.js
+++ budget.js
@@ -208,9 +208,6 @@ if (Meteor.isClient) { //THE ARRAY ISN'T GOING INTO THE FUNCTION PROPERLY SO THA
       Session.set("mode", mode);
 			thisPeriodStart = thisWeekStart;
 			thisPeriodEnd = thisWeekEnd;
-      //Show this section as active
-      $(".summary-box").removeClass("active");
-      $("#weekBox").addClass("active");
       setTitleDates();
 			filterDates();
 		},
@@ -219,10 +216,6 @@ if (Meteor.isClient) { //THE ARRAY ISN'T GOING INTO THE FUNCTION PROPERLY SO THA
       Session.set("mode", mode);
 			thisPeriodStart = thisMonthStart;
 			thisPeriodEnd = thisMonthEnd;
-      //Show this section as active
-      $(".summary-box").removeClass("active");
-      $("#monthBox").addClass("active");
-
       setTitleDates();
 			filterDates();
 		},	
@@ -231,9 +224,6 @@ if (Meteor.isClient) { //THE ARRAY ISN'T GOING INTO THE FUNCTION PROPERLY SO THA
       Session.set("mode", mode);
 			thisPeriodStart = allTimeStart;
 			thisPeriodEnd = allTimeEnd;
-      //Show this section as active
-      $(".summary-box").removeClass("active");
-      $("#allTimeBox").addClass("active");
       setTitleDates();
 			filterDates();
 		},		
